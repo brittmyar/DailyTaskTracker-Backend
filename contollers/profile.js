bf7f4f43492ecd893b1f56709b2e1201480c52fa
Brittany Yarbrough <brittanymyar@gmail.com>
2023-01-05 13:07:48 -0500

    set up delete and update route

--- contollers/profile.js
+++ contollers/profile.js
@@ -12,3 +12,49 @@ profileRouter.get('/taskPage', async (req, res) => {
         res.status(400).json(error);
     }
 });
+
+// Profile/task Create route
+profileRouter.post('/taskPage', async (req, res) => {
+    try {
+        res.json(await Task.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+
+// Profile/task Delete route
+profileRouter.delete("/task/:id", async (req, res) => {
+    try {
+        // send all task
+        res.json(await Task.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+});
+
+// Profile/task Update route
+profileRouter("/task/:id", async (req, res) => {
+    try {
+        // send all task
+        res.json(
+            await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        );
+    } catch (error) {
+        //send error
+        res.status(400).json(error);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = profileRouter;
